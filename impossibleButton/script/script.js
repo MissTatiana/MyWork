9b87d0e4855da9cf8dd78058142f99fe1a24c6de
Tatianae <Miss_Tati_K@yahoo.com>
2013-05-30 22:05:36 -0400

    alert added for yes click

--- impossibleButton/script/script.js
+++ impossibleButton/script/script.js
@@ -9,4 +9,8 @@ var yes = document.getElementById("yes");
 yes.onmouseover = function(){
 	yes.style.marginLeft = (Math.random()*200) + "px";
 	yes.style.top = (Math.random()*100) + "px";
+
+	if(yes.onclick){
+		alert("You sneeky liar!")
+	}
 }
